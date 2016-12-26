bf98ca331c44f9e0d92599ba97afff8a87a6f58d
d-serj <sp11kenny@gmail.com>
2016-12-26 11:41:15 +0200

    Update integers_counter.pas

--- integers_counter.pas
+++ integers_counter.pas
@@ -3,23 +3,22 @@ Program integers_counter;
 var x, n, p, sum: integer;
 
 begin
-	n:= 0;
-	p:= 0;
-	sum:= 0;
+	n   := 0;
+	p   := 0;
+	sum := 0;
 
 	writeln('Input positive or negative integers, 0 to end: ');
 
 	repeat
 		readln(x);
-		if x < 0
-		then inc(n);
+		if x < 0 then inc(n);
 		if x > 0 then
 			begin
 				inc(p);
-				sum:= sum + x;
+				sum := sum + x;
 			end
 	until (x = 0);
-	
+
 	writeln('Number of positive integers: ', p);
 	writeln('Number of negative integers: ', n);
 	writeln('Sum of positive integers is: ', sum);
